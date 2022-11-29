82d654fd1113d2739c0a56835d2986556aced3d1
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-11-28 19:58:02 -0500

    Implemented login functionality

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ app.use(cors());
 app.use('/users', userRoutes)
 require('./connection')
 
+const server = require('http').createServer(app);
+const PORT = 5001;
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
 
 server.listen(PORT, ()=> {
   console.log('listening to port', PORT)
