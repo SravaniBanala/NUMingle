f090133317230f9858791980f44b80e1a2009035
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-01 14:50:03 -0500

    cteated chat ui

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react";
+import { Col, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { AppContext } from "../context/appContext";
+import { addNotifications, resetNotifications } from "../features/userSlice";
+import "./Sidebar.css";
+
+function Sidebar() {
+    console.log("in Sidebar")
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const { socket, setMembers, members, setCurrentRoom, setRooms, privateMemberMsg, rooms, setPrivateMemberMsg, currentRoom } = useContext(AppContext);
+    //to toggle sidebar for chat and lobby
+    const { isChat, isLobby } = useContext(AppContext);
+
+
+    return (
+        <>
+            {isLobby && 
+            <ListGroup>
+                {rooms.map((room, idx) => (
+                    <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room == currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" }}>
+                        {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+            }
+
+
+        </>
+    );
+}
+
+export default Sidebar;
