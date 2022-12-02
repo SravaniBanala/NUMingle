fe9ff486fb7023d08a185cfed59f0c7fe951d104
SravaniBanala2 <banala.sr@northeastern.edu>
2022-12-01 21:18:14 -0500

    chat functionality server.js changes

--- server/server.js
+++ server/server.js
@@ -29,6 +29,12 @@ require('./connection')
 
 const server = require('http').createServer(app);
 const PORT = 5001;
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
 
 
 
