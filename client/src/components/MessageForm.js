8cef080cbc6e4195362c858d6d9067aec7971eb7
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-03 13:55:32 -0500

    modified chat side bar

---
+++ client/src/components/MessageForm.js
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { AppContext } from "../context/appContext";
+import "./MessageForm.css";
+function MessageForm() {
+    console.log("in MessageForm")
+    const [message, setMessage] = useState("");
+    const user = useSelector((state) => state.user);
+    const { socket, currentRoom, setMessages, messages, privateMemberMsg } = useContext(AppContext);
+    const messageEndRef = useRef(null);
+    
+    return (
+        <>
+            <div className="messages-output">
+                {user && !privateMemberMsg?._id && <div className="alert alert-info">You are in the {currentRoom} room</div>}
+                {user && privateMemberMsg?._id && (
+                    <>
+                        <div className="alert alert-info conversation-info">
+                            <div>
+                                Your conversation with {privateMemberMsg.name} <img src={privateMemberMsg.picture} className="conversation-profile-pic" />
+                            </div>
+                        </div>
+                    </>
+                )}
+                {!user && <div className="alert alert-danger">Please login</div>}
+
+                {user &&
+                    messages.map(({ _id: date, messagesByDate }, idx) => (
+                        <div key={idx}>
+                            <p className="alert alert-info text-center message-date-indicator">{date}</p>
+                            {messagesByDate?.map(({ content, time, from: sender }, msgIdx) => (
+                                <div className={sender?.email == user?.email ? "message" : "incoming-message"} key={msgIdx}>
+                                    <div className="message-inner">
+                                        <div className="d-flex align-items-center mb-3">
+                                            <img src={sender.picture} style={{ width: 35, height: 35, objectFit: "cover", borderRadius: "50%", marginRight: 10 }} />
+                                            <p className="message-sender">{sender._id == user?._id ? "You" : sender.name}</p>
+                                        </div>
+                                        <p className="message-content">{content}</p>
+                                        <p className="message-timestamp-left">{time}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                <div ref={messageEndRef} />
+            </div>
+        </>
+    );
+}
+
+export default MessageForm;
