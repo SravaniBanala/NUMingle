1ac6154405d0630380777505e0854853c9ae42ec
adatiyan <adatiya.n@northeastern.edu>
2022-12-03 13:28:39 -0500

    Adding conext of Signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -15,51 +15,9 @@ function Signup() {
     const [signupUser, { isLoading, error }] = useSignupUserMutation();
     const { loggedinAs, setLoggedinAs } = useContext(AppContext);
     const navigate = useNavigate();
-    //image upload states
-    const [image, setImage] = useState(null);
-    const [upladingImg, setUploadingImg] = useState(false);
-    const [imagePreview, setImagePreview] = useState(null);
-
-    function validateImg(e) {
-        const file = e.target.files[0];
-        if (file.size >= 1048576) {
-            return alert("Max file size is 1mb");
-        } else {
-            setImage(file);
-            setImagePreview(URL.createObjectURL(file));
-        }
-    }
-
-    async function uploadImage() {
-        const data = new FormData();
-        if (!image) {
-            data.append("file", profile_img);
-        }
-        else {
-            data.append("file", image);
-        }
-        data.append("upload_preset", "nu_mingle");
-        try {
-            setUploadingImg(true);
-            let res = await fetch("https://api.cloudinary.com/v1_1/dwacjep8p/image/upload", {
-                method: "post",
-                body: data,
-            });
-            const urlData = await res.json();
-            setUploadingImg(false);
-            console.log("URL -->", urlData.url)
-            return urlData.url;
-        } catch (error) {
-            setUploadingImg(false);
-            console.log(error);
-        }
-    }
-
+    
     async function handleSignup(e) {
         e.preventDefault();
-        //if (!image) return alert("Please upload your profile picture");
-        const url = await uploadImage(image);
-        console.log(url);
         // signup the user
         signupUser({ name, nuid, email, password, picture: url, type }).then(({ data }) => {
             if (data) {
@@ -87,13 +45,7 @@ function Signup() {
                 <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
                     <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleSignup}>
                         <h2 className="text-center">Create account</h2>
-                        <div className="signup-profile-pic__container">
-                            <img src={imagePreview || profile_img} className="signup-profile-pic" />
-                            <label htmlFor="image-upload" className="image-upload-label">
-                                <i className="fas fa-plus-circle add-picture-icon"></i>
-                            </label>
-                            <input type="file" id="image-upload" hidden accept="image/png, image/jpeg image/jpg" onChange={validateImg} />
-                        </div>
+                       
                         {/* <div className="signup-dropdown">
                         <DropdownButton id="dropdown-basic-button" title={loggedinAs}>
                             <Dropdown.Item onClick={()=>{loginStudentHandler()}}>Student</Dropdown.Item>
