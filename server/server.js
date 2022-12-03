b4664fc3bf785454fc416ff88b505f5f0cd64fa6
SravaniBanala2 <banala.sr@northeastern.edu>
2022-12-03 12:16:35 -0500

    integrating server.js

--- server/server.js
+++ server/server.js
@@ -36,6 +36,27 @@ const io = require('socket.io')(server, {
   }
 })
 
+
+async function getLastMessagesFromRoom(room){
+  let roomMessages = await Message.aggregate([
+    {$match: {to: room}},
+    {$group: {_id: '$date', messagesByDate: {$push: '$$ROOT'}}}
+  ])
+  return roomMessages;
+}
+
+function sortRoomMessagesByDate(messages){
+  return messages.sort(function(a, b){
+    let date1 = a._id.split('/');
+    let date2 = b._id.split('/');
+
+    date1 = date1[2] + date1[0] + date1[1]
+    date2 =  date2[2] + date2[0] + date2[1];
+
+    return date1 < date2 ? -1 : 1
+  })
+}
+
 // socket connection
 
 io.on('connection', (socket)=> {
