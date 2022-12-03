a1cabc8c903cb44a832d2505c91d45e0c71cb7f0
SravaniBanala2 <banala.sr@northeastern.edu>
2022-12-02 21:53:52 -0500

    changes message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-  content: String,
-  from: Object
 })
 
 const Message = mongoose.model('Message', MessageSchema);
