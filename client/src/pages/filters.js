26a9ca9ed77d00457acf46199318f0f44a00d298
SravaniBanala2 <banala.sr@northeastern.edu>
2022-12-04 11:04:35 -0500

    Admin All students Frontend Filter

--- client/src/pages/filters.js
+++ client/src/pages/filters.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import { Input, CustomInput } from 'reactstrap';
 
-
+export const Filter = ({ column }) => {
+  return (
+    <div style={{ marginTop: 5 }}>
+      {column.canFilter && column.render('Filter')}
+    </div>
+  );
+};
+  // return (
+  //   // <Input
+  //   //   id='custom-select'
+  //   //   type='select'
+  //   //   value={filterValue}
+  //   //   onChange={(e) => {
+  //   //     setFilter(e.target.value || undefined);
+  //   //   }}
+  //   // >
+  //   //   <option value=''>All</option>
+  //   //   {options.map((option) => (
+  //   //     <option key={option} value={option}>
+  //   //       {option}
+  //   //     </option>
+  //   //   ))}
+  //   // </Input>
+  // );
 };
