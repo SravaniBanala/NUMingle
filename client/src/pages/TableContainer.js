f33ef3803c6424b8c9df7ed25752a154f1cf045f
SravaniBanala2 <banala.sr@northeastern.edu>
2022-12-04 07:55:14 -0500

    Admin All students pagination changes

--- client/src/pages/TableContainer.js
+++ client/src/pages/TableContainer.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react';
 import {
   useTable,
+  useSortBy,
+  useFilters,
+  useExpanded,
   usePagination,
 } from 'react-table';
 import { Table, Row, Col, Button, Input, CustomInput } from 'reactstrap';
+import { Filter, DefaultColumnFilter } from './filters';
 
 const TableContainer = ({ columns, data, renderRowSubComponent }) => {
   const {
@@ -16,17 +20,37 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
     canPreviousPage,
     canNextPage,
     pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
     setPageSize,
     state: { pageIndex, pageSize },
   } = useTable(
     {
       columns,
       data,
+      defaultColumn: { Filter: DefaultColumnFilter },
       initialState: { pageIndex: 0, pageSize: 10 },
     },
+    useFilters,
+    useSortBy,
     useExpanded,
     usePagination
   );
+
+  const generateSortingIndicator = (column) => {
+    return column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : '';
+  };
+
+  const onChangeInSelect = (event) => {
+    setPageSize(Number(event.target.value));
+  };
+
+  const onChangeInInput = (event) => {
+    const page = event.target.value ? Number(event.target.value) - 1 : 0;
+    gotoPage(page);
+  };
 
   return (
     <Fragment>
@@ -62,6 +86,7 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
                 {row.isExpanded && (
                   <tr>
                     <td colSpan={visibleColumns.length}>
+                      {renderRowSubComponent(row)}
                     </td>
                   </tr>
                 )}
@@ -94,8 +119,42 @@ const TableContainer = ({ columns, data, renderRowSubComponent }) => {
             {pageIndex + 1} of {pageOptions.length}
           </strong>
         </Col>
-
-
+        <Col md={2}>
+          <Input
+            type='number'
+            min={1}
+            style={{ width: 70 }}
+            max={pageOptions.length}
+            defaultValue={pageIndex + 1}
+            onChange={onChangeInInput}
+          />
+        </Col>
+        <Col md={2}>
+          <Input
+            type='select'
+            value={pageSize}
+            onChange={onChangeInSelect}
+          >
+          
+            {[5, 10, 15, 20, 25].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </Input>
+        </Col>
+        <Col md={3}>
+          <Button color='primary' onClick={nextPage} disabled={!canNextPage}>
+            {'>'}
+          </Button>
+          <Button
+            color='primary'
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {'>>'}
+          </Button>
+        </Col>
       </Row>
     </Fragment>
   );
