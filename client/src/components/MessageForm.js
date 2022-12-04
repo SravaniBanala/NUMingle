7a09ea18eebe3a10eb2792fd4bf61fd672674d11
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-04 16:06:22 -0500

    Added status in chat bar

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -85,6 +85,20 @@ function MessageForm() {
                     ))}
                 <div ref={messageEndRef} />
             </div>
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col md={11}>
+                        <Form.Group>
+                            <Form.Control type="text" placeholder="Your message" disabled={!user} value={message} onChange={(e) => setMessage(e.target.value)}></Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col md={1}>
+                        <Button variant="primary" type="submit" style={{ width: "100%", backgroundColor: "orange" }} disabled={!user}>
+                            <i className="fas fa-paper-plane"></i>
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
         </>
     );
 }
