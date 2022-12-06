3bc24d409344d4c187478fa4c151b6c0b63fecce
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-06 18:09:56 -0500

    resolved polls errors

--- client/src/components/PollsChart.js
+++ client/src/components/PollsChart.js
@@ -26,7 +26,26 @@ function PollsChart({curPoll}) {
 
       <Bar
         data={{
-         
+          labels: labelsArr,
+          datasets: [
+            {
+              label: '# of votes',
+              data: dataArr,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
         }}
         height={300}
         width={300}
