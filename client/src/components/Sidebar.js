bbcf8aa671b73c0dbafe76e354bc83c3c2dacddb
adatiyan <adatiya.n@northeastern.edu>
2022-12-06 15:26:21 -0500

    Representing and formign side bar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,82 @@
+import React, { useContext, useEffect } from "react";
+import { Col, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { AppContext } from "../context/appContext";
+import { addNotifications, resetNotifications } from "../features/userSlice";
+import "./Sidebar.css";
+
+function Sidebar() {
+    console.log("in Sidebar")
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const { socket, setMembers, members, setCurrentRoom, setRooms, privateMemberMsg, rooms, setPrivateMemberMsg, currentRoom } = useContext(AppContext);
+    //to toggle sidebar for chat and lobby
+    const { isChat, isLobby } = useContext(AppContext);
+
+    function joinRoom(room, isPublic = true) {
+        if (!user) {
+            return alert("Please login");
+        }
+        
+        if (isPublic) {
+            setPrivateMemberMsg(null);
+        }
+        // dispatch for notifications
+        dispatch(resetNotifications(room));
+    }
+
+   
+
+    useEffect(() => {
+        if (user) {
+            setCurrentRoom("general");
+            getRooms();
+            socket.emit("join-room", "general");
+            socket.emit("new-user");
+        }
+    }, []);
+
+   
+
+    
+
+    if (!user) {
+        return <></>;
+    }
+    return (
+        <>
+            {isLobby && 
+            <ListGroup>
+                {rooms.map((room, idx) => (
+                    <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room == currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" }}>
+                        {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+            }
+            {isChat && 
+            members.map((member) => (
+                <ListGroup.Item key={member.id} style={{ cursor: "pointer" }} active={privateMemberMsg?._id == member?._id} onClick={() => handlePrivateMemberMsg(member)} hidden={member._id === user._id}>
+                    <Row>
+                        <Col xs={2} className="member-status">
+                            <img src={member.picture} className="member-status-img" />
+                            {member.status == "online" ? <i className="fas fa-circle sidebar-online-status"></i> : <i className="fas fa-circle sidebar-offline-status"></i>}
+                        </Col>
+                        <Col xs={9}>
+                            {member.name}
+                            {member._id === user?._id && " (You)"}
+                            {member.status == "offline" && " (Offline)"}
+                        </Col>
+                        <Col xs={1}>
+                            <span className="badge rounded-pill bg-primary">{user.newMessages[orderIds(member._id, user._id)]}</span>
+                        </Col>
+                    </Row>
+                </ListGroup.Item>
+            ))
+            }
+
+        </>
+    );
+}
+
+export default Sidebar;
