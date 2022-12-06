dd220d4c844cc8084334a4c2dec047fe399e4e80
adatiyan <adatiya.n@northeastern.edu>
2022-12-06 13:07:11 -0500

    Modifying the features

--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,25 @@ const UserSchema = new mongoose.Schema({
     type: String,
     //required: [true, "Can't be blank"]
   },
-  
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, "Can't be blank"],
+    index: true,
+    validate: [isEmail, "invalid email"]
+  },
+  password: {
+    type: String,
+    required: [true, "Can't be blank"]
+  },
+  picture: {
+    type: String,
+  },
+  newMessages: {
+    type: Object,
+    default: {}
+  },
   status: {
     type: String,
     default: 'online'
